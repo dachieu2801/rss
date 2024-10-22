cd73d2b47d1c39861ac19718bf29e8209baf788d
dachieu2801 <ankhieu322@gmail.com>
2024-10-22 22:20:12 +0700

    hieu

--- server.js
+++ server.js
@@ -5,14 +5,41 @@ const cron = require('node-cron');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
+
+// app.get('/', async (req, res) => {
+//     const articles = await crawlWebsite();
+//     const rssFeed = createRssFeed(articles);
+//     fs.writeFileSync('feed.xml', rssFeed); 
+//     res.set('Content-Type', 'application/rss+xml');
+//     res.send(rssFeed);
+// });
 
 app.get('/', async (req, res) => {
-    const articles = await crawlWebsite();
-    const rssFeed = createRssFeed(articles);
-    fs.writeFileSync('feed.xml', rssFeed); 
-    res.set('Content-Type', 'application/rss+xml');
-    res.send(rssFeed);
+    const response = await fetch('https://community.lexinfocus.com/api/52831/sign-in',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: 'ankhieu322@gmail.com',
+            password: 'hieuha2801',
+        }),
+    });
+
+    
+    const loginJson = await response.json();
+
+    const getFeed = await fetch('https://community.lexinfocus.com/api/52831/threads?c=null&f=null',{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Estage-Authorization': loginJson?.user?.access_token || ""
+        },
+    });
+    const feed = await getFeed.json();
+    const authFeeds = feed.threads.filter((item) => item.author?.uid == loginJson?.user?.id);
+    res.json(authFeeds);
 });
 
 // // Cron job để crawl nội dung định kỳ
